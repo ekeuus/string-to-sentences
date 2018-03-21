7cdfd2f4ca1a4f178e6edd9effb0d4dc46adfebf
Ekke <ekke@tdl.ee>
2018-03-21 15:59:31 +0200

    :new: Package 1.0 release

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const regex = /\S.*?(\.|\?|\!)"?(?=\s|$)/g;
+const punctuationRegex = /[^0-9a-z ]/gi;
+
+function removePunctuationFromText(sentence) {
+	return sentence.replace(punctuationRegex, '');
+}
+
+function getSentences(string, options = { removePunctuation: false }) {
+	if (typeof string === 'string' || string instanceof String) {
+		const { removePunctuation } = options;
+		const result = string.match(regex);
+		if (!result.length) {
+			const initial = removePunctuation ?
+				removePunctuationFromText(string) :
+				string;
+			return [initial];
+		}
+		return removePunctuation ?
+			result.map(removePunctuationFromText) :
+			result;
+	} else {
+		return [];
+	}
+}
+
+export default {
+	getSentences,
+}
